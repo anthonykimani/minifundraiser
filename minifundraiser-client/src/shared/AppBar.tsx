624d062f301d590327ce846d26893a5d7aea7178
anthonykimani <kimaniantoni@gmail.com>
2024-03-15 01:17:29 +0300

    hotfix: removes useWeb3Modal hook provider

--- minifundraiser-client/src/shared/AppBar.tsx
+++ minifundraiser-client/src/shared/AppBar.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { BigNumber } from "bignumber.js";
-import { useWeb3Modal } from "@web3modal/wagmi/react";
+// import { useWeb3Modal } from "@web3modal/wagmi/react";
 
 const AppBar = () => {
-  const { open } = useWeb3Modal();
+  // const { open } = useWeb3Modal();
   return (
     <div className="bg-gradient-to-r from-[#E99123] to-[#FFBC6B] py-4 px-2 flex items-center justify-between">
       <h2 className="text-white">MiniFundraiser</h2>
 
-      <button onClick={() => open()} className="py-3 px-6 border border-white bg-[#E99123] rounded-full text-white hover:cursor-pointer">
+      {/* onClick={() => open()}*/}
+      <button className="py-3 px-6 border border-white bg-[#E99123] rounded-full text-white hover:cursor-pointer">
         Connect Wallet
       </button>
     </div>
