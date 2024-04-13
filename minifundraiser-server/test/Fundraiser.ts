22639f0f03163c8c4fb49999898ea1a6bd253710
anthonykimani <kimaniantoni@gmail.com>
2024-04-13 10:29:40 +0300

    testing: Adds Fundraiser.ts file to test the smart contract docs: Adds a ReadMe File to track project changes

--- minifundraiser-server/test/Fundraiser.ts
+++ minifundraiser-server/test/Fundraiser.ts
@@ -1,30 +1,68 @@
 import { expect } from "chai";
 import { viem } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { toHex, hexToString, fromHex } from "viem";
+import { toHex, hexToString, fromHex, parseEther } from "viem";
 
 async function fixture() {
   const publicClient = await viem.getPublicClient();
-  const [deployer, account1, account2] = await viem.getWalletClients();
+  const [deployer, account1, account2, account3] =
+    await viem.getWalletClients();
   const fundraiser = await viem.deployContract("Fundraiser", []);
-  return { fundraiser, publicClient, deployer, account1, account2 };
+  return { fundraiser, publicClient, deployer, account1, account2, account3 };
 }
 
 describe("Minifundraiser on Minipay", async () => {
   describe("When the Contract is Deployed", async () => {
     it("User can Create Fundraising Campaign with name, targetAmount, campaignAddress, campaignDeadline", async () => {
-      const { fundraiser, deployer, account1, account2 } = await loadFixture(
-        fixture
-      );
-      await fundraiser.write.createCampaign([
+      const {
+        fundraiser,
+        deployer,
+        account1,
+        account2,
+        account3,
+        publicClient,
+      } = await loadFixture(fixture);
+      const Water4LifeTx = await fundraiser.write.createCampaign([
         toHex("Water4Life Borehole Project", { size: 32 }),
-        2000n,
+        300000n,
         account1.account.address,
         1711670400n,
       ]);
-      const campaign1 = await fundraiser.read.campaigns([BigInt(0)]);
-      console.log(campaign1);
-      expect(campaign1).to.exist;
+      const Water4LifetxReceipt = await publicClient.getTransactionReceipt({
+        hash: Water4LifeTx,
+      });
+      const Electricity4SchoolsTx = await fundraiser.write.createCampaign([
+        toHex("Electricity For Schools", { size: 32 }),
+        500000n,
+        account1.account.address,
+        1711670400n,
+      ]);
+      const Electricity4SchoolsTxReceipt =
+        await publicClient.getTransactionReceipt({
+          hash: Electricity4SchoolsTx,
+        });
+
+      const EnvironmentalTx = await fundraiser.write.createCampaign([
+        toHex("Environmental Reclaimation", { size: 32 }),
+        100000n,
+        account1.account.address,
+        1711670400n,
+      ]);
+      const EnvironmentalTxReceipt = await publicClient.getTransactionReceipt({
+        hash: EnvironmentalTx,
+      });
+      const campaign0 = await fundraiser.read.campaigns([BigInt(0)]);
+      expect(campaign0[0]).to.equal(
+        toHex("Water4Life Borehole Project", { size: 32 })
+      );
+      const campaign1 = await fundraiser.read.campaigns([BigInt(1)]);
+      expect(campaign1[0]).to.equal(
+        toHex("Electricity For Schools", { size: 32 })
+      );
+      const campaign2 = await fundraiser.read.campaigns([BigInt(2)]);
+      expect(campaign2[0]).to.equal(
+        toHex("Environmental Reclaimation", { size: 32 })
+      );
     });
   });
 });
